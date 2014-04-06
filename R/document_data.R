285c7a6fe8be8ccd5e96bd683c0eafb272aa2cf7
shelly1436@gmail.com <shelly1436@gmail.com>
2014-04-05 23:11:06 -0400

    document myDiff

---
+++ R/document_data.R
@@ -0,0 +1,9 @@
+#' Example myDiff object.
+#' 
+#' This data set includes example of the following objects:
+#'  \code{myDiff}. You can load the data using \code{data(edmr)}
+#'
+#' @name myDiff
+#' @docType data
+#' @format  data.frame
+NULL
